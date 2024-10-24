688ba38b9d21bfe49fc8fb732bccd9a12e029da6
Paul Guillen <paulguillen190@gmail.com>
2024-10-23 21:46:10 -0600

    Restart carrousel when prev and next action is clicked

--- src/features/client/components/BannerSection.tsx
+++ src/features/client/components/BannerSection.tsx
@@ -1,41 +1,54 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { CarouselImage } from "../../../features/model/CarouselImage";
 import { CAROUSEL_INTERVAL } from "../../../utils/Constants";
 import "../../../styles/BannerSection.css";
 import { fetchCarouselImages } from "../services/HomeService";
 
-
 const BannerSection = () => {
   const [images, setImages] = useState<CarouselImage[]>([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
     const loadImages = async () => {
       const fetchedImages = await fetchCarouselImages();
       setImages(fetchedImages);
-      console.log(fetchedImages);
     };
 
     loadImages();
   }, []);
 
-  const handleNext = useCallback(() => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  const restartInterval = useCallback(() => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+
+    intervalRef.current = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, CAROUSEL_INTERVAL);
   }, [images.length]);
 
-  const handlePrev = () => {
+  const handleNext = useCallback(() => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    restartInterval();
+  }, [images.length, restartInterval]);
+
+  const handlePrev = useCallback(() => {
     setCurrentIndex(
       (prevIndex) => (prevIndex - 1 + images.length) % images.length
     );
-  };
+    restartInterval();
+  }, [images.length, restartInterval]);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      handleNext();
-    }, CAROUSEL_INTERVAL);
+    restartInterval();
 
-    return () => clearInterval(interval);
-  }, [handleNext]);
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, [restartInterval]);
 
   return (
     <section className="banner-section">
