a8282b34951e6c2bc9d10363c3de188807a9a573
Paul Guillen <paulguillen190@gmail.com>
2024-11-01 20:27:42 -0600

    Login updated, styles

--- src/features/auth/pages/client/login/Login.tsx
+++ src/features/auth/pages/client/login/Login.tsx
@@ -2,25 +2,25 @@ import "../../../../../styles/auth/client/Login.css";
 
 export const Login = () => {
     return (
-        <div className="login-container">
-          <h2>Iniciar sesión</h2>
-          <p>¡Bienvenido a Ripley.com!</p>
-          <form>
-            <label>
-              Correo o DNI
-              <input type="text" placeholder="Correo o DNI*" />
-            </label>
-            <label>
-              Contraseña
-              <input type="password" placeholder="Contraseña*" />
-            </label>
-            <button type="submit">Iniciar sesión</button>
-            <button type="button">Iniciar sesión con Google</button>
-            <button type="button">Iniciar sesión con Apple</button>
-          </form>
-          <p>¿Eres nuevo en Ripley.com? <a href="/signUpClient">Crear cuenta</a></p>
+        <div className="main-container">
+            <div className="login-container">
+                <h2>Iniciar sesión</h2>
+                <p>¡Bienvenido a Ripley.com!</p>
+                <form>
+                    <label>
+                        <input type="text" placeholder="Correo o DNI*" />
+                    </label>
+                    <label>
+                        <input type="password" placeholder="Contraseña*" />
+                    </label>
+                    <button type="submit">Iniciar sesión</button>
+                    <button type="button">Iniciar sesión con Google</button>
+                    <button type="button">Iniciar sesión con Apple</button>
+                </form>
+                <p>¿Eres nuevo? <a href="/signUpClient">Crear cuenta</a></p>
+            </div>
         </div>
-      );
+    );
 };
 
 export default Login;
