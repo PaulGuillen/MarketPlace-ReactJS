99de7f810d6902b110a1182c40d67fd928500890
Paul Guillen <paulguillen190@gmail.com>
2025-01-03 21:24:18 -0600

    Change styles in home section best viral products and stores

--- src/features/client/components/product/BestSellersSection.tsx
+++ src/features/client/components/product/BestSellersSection.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { fetchBestSellers } from "../../services/HomeService";
-import "../../../../styles/BestSellersSections.css";
 import { Product } from "../../../model/Product";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
@@ -28,54 +27,71 @@ const BestSellersSection = () => {
   };
 
   return (
-    <section className="best-sellers-section">
-      <h2>Los Más Vendidos</h2>
-      <p>Encuentra los productos favoritos de belleza</p>
-      <div className="best-sellers-list">
-        {bestSellers.map((item) => {
-          const showDiscount =
-            item.discount !== "0" && item.priceWithDiscount !== "0";
+    <section className="pb-10 bg-gray-100">
+      <div className="container mx-auto px-4">
+        <h2 className="text-2xl font-bold text-gray-800 mb-4 text-center">
+          Los Más Vendidos
+        </h2>
+        <div className="flex items-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {bestSellers.map((item) => {
+              const showDiscount =
+                item.discount !== "0" && item.priceWithDiscount !== "0";
 
-          return (
-            <div
-              key={item.id}
-              className="best-seller-card"
-              onClick={() => handleProductClick(item)}
-            >
-              <img
-                src={item.imageUrl}
-                alt={item.name}
-                className="product-image"
-              />
-              <h3>{item.name}</h3>
-              <p>{item.description}</p>
-              <div className="price-container">
-                {showDiscount ? (
-                  <>
-                    <span className="original-price">S/ {item.price}</span>
-                    <span className="discounted-price">
-                      S/ {item.priceWithDiscount}
-                    </span>
-                  </>
-                ) : (
-                  <span className="product-price">S/ {item.price}</span>
-                )}
-              </div>
-              {showDiscount && (
-                <span className="discount-badge">-{item.discount}%</span>
-              )}
-              <button
-                className="add-to-cart-button"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handleAddToCart(item);
-                }}
-              >
-                Agregar al carrito
-              </button>
-            </div>
-          );
-        })}
+              return (
+                <div
+                  key={item.id}
+                  className="relative bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-shadow cursor-pointer"
+                  onClick={() => handleProductClick(item)}
+                >
+                  <div className="relative">
+                    {showDiscount && (
+                      <span className="absolute top-0 left-0 bg-red-500 text-white text-xs font-bold rounded-br-lg px-4 py-2 flex items-center justify-center">
+                        -{item.discount}%
+                      </span>
+                    )}
+                    <img
+                      src={item.imageUrl}
+                      alt={item.name}
+                      className="w-[271px] h-[186px] object-cover"
+                    />
+                  </div>
+                  <div className="p-4">
+                    <h3 className="text-lg font-semibold text-gray-800">
+                      {item.name}
+                    </h3>
+                    <p className="text-gray-600 text-sm">{item.description}</p>
+                    <div className="mt-4">
+                      {showDiscount ? (
+                        <>
+                          <span className="text-gray-500 line-through mr-2">
+                            S/ {item.price}
+                          </span>
+                          <span className="text-green-500 font-bold">
+                            S/ {item.priceWithDiscount}
+                          </span>
+                        </>
+                      ) : (
+                        <span className="text-gray-800 font-bold">
+                          S/ {item.price}
+                        </span>
+                      )}
+                    </div>
+                    <button
+                      className="mt-4 w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleAddToCart(item);
+                      }}
+                    >
+                      Agregar al carrito
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/features/client/components/store/StoresSection.tsx
+++ src/features/client/components/store/StoresSection.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import { fetchStores } from "../../services/HomeService";
 import { Store } from "../../../model/Store";
-import "../../../../styles/StoreSection.css";
-import Divider from "../../../../components/divider/Divider";
 
 const StoresSection = () => {
   const [stores, setStores] = useState<Store[]>([]);
@@ -11,31 +9,43 @@ const StoresSection = () => {
     const loadStores = async () => {
       const fetchedStores = await fetchStores();
       setStores(fetchedStores);
-
     };
 
     loadStores();
   }, []);
 
-  /**NEED LOGIC TO VALIDATE IF BUSINES HAVE A BRAND OR NOT? */
   return (
-    <div className="store-section">
-      <h2 className="store-title">Nuestras Tiendas</h2>
-      <p className="store-description">
-        Encuentra nuestras tiendas a lo largo de Perú
-      </p>
-      <div className="store-list">
-        {stores.map((store, index) => (
-          <div className="store-card" key={index}>
-            <div className="store-image-container">
-              <img src={store.logoImage} alt={store.businessName} className="store-image" />
+    <section className="py-10 bg-gray-100">
+      <div className="container mx-auto px-4">
+        <h2 className="text-2xl font-bold text-gray-800 mb-4 text-center">
+          Nuestras Tiendas
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+          {stores.map((store, index) => (
+            <div
+              key={index}
+              className="relative bg-blue-100 rounded-lg shadow-md hover:shadow-lg transition-shadow cursor-pointer flex items-center px-8 py-10 max-w-lg"
+            >
+              <div className="flex-1 flex flex-col">
+                <h3 className="font-bold text-gray-800 mb-4">
+                  {store.businessName}
+                </h3>
+                <p className="text-blue-600 font-medium hover:underline cursor-pointer mt-8">
+                  Comprar ahora
+                </p>
+              </div>
+              <div className="flex-shrink-0 ml-2">
+                <img
+                  src={store.logoImage}
+                  alt={store.businessName}
+                  className="w-28 h-28 object-contain rounded-lg"
+                />
+              </div>
             </div>
-            <Divider height="2px" />
-            <h3 className="store-title">{store.businessName}</h3>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
